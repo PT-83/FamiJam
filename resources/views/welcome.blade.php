97ae53ed69008e14ed35bae811cb84104659c913
PT-83 <paolo.g.trulli@gmail.com>
2020-05-15 21:45:29 -0400

    added demo video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
     </div>
 </div>
 
-<div class="container h-100">
+<div class="container h-100 pb-5">
     <div class="row h-100 justify-content-center align-items-center">
         <div class="col">
             <h3 class="mt-3 text-secondary">What kind of things should you submit? Our answer, anything you desire. Homemade recipes, favourite jokes, stories, experiences, photographs and alike.
@@ -40,6 +40,10 @@
         </div>
     </div>
 </div>
+
+<h2 class="text-center align-center text-secondary">Demo Video</h2>
+<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/f9996e35d02f4c73afa6d08f2d0e6575" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+
 
 <div class="container h-100 mt-5">
     <div class="row h-100 justify-content-center align-items-center">
