e369b6d358776fe45e3bee6a78e38559287d0ce4
PT-83 <paolo.g.trulli@gmail.com>
2020-07-03 14:27:48 -0400

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,15 @@
     }
     </style>
     
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZBYSTKXH97"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZBYSTKXH97');
+</script>
 
 </head>
 <body>
